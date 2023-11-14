567d8018b4f6783fd62dd6d69059e79849ca1c80
Lord-Eler <80069231+Lord-Eler@users.noreply.github.com>
2023-11-14 17:27:11 +0100

    end of lab02

--- lab01/PointSet.py
+++ lab01/PointSet.py
@@ -41,7 +41,8 @@ class PointSet:
         self.types = types
         self.features = np.array(features)
         self.labels = np.array(labels)
-    
+        self.min_split_points = 1
+
     def get_gini(self) -> float:
         """Computes the Gini score of the set of points
 
@@ -141,36 +142,47 @@ class PointSet:
                 
             elif self.types[feature_index] == FeaturesTypes.REAL:
                 #Split if the feature is real (according to the method explained in the subject)
+                #We use a method get_gini_2
                 #We don't save every gain for every value, but only the value of each real feature maximizing the gain (exactly as for categorical features)
-                continuous_values = sorted(self.features[:,feature_index])
+                continuous_values = sorted([[self.features[k][feature_index], self.labels[k]] for k in range(len(self.features))], key = lambda point : point[0])
                 current_max_gini = 0
                 current_value_maximizing = 0
-                for value in continuous_values:
-                    child1_features = []
-                    child2_features = []
-                    child1_labels = []
-                    child2_labels = []
-                    #We can now do the split
-                    for i,f in enumerate(self.features):
-                        if f[feature_index] < value:
-                            child1_features.append(f)
-                            child1_labels.append(self.labels[i])
-                        else:
-                            child2_features.append(f)
-                            child2_labels.append(self.labels[i])
-                    if len(child1_features) < self.min_split_points or len(child2_features) < self.min_split_points:
+                gini_start = self.get_gini()
+                label_counter = {'left_false': 0, 'left_true': 0, 'right_false': 0, 'right_true': 0}
+                true_number = sum(self.labels)
+                label_counter['right_true'] = true_number
+                label_counter['right_false'] = n - true_number
+                threshold = 1
+                while threshold < n: #We don't need to check when one of the sets is empty   
+                    current_label = continuous_values[threshold-1][1]
+                    if current_label:
+                        label_counter['left_true'] += 1
+                        label_counter['right_true'] -= 1
+                    else:
+                        label_counter['left_false'] += 1
+                        label_counter['right_false'] -= 1
+                    
+                    #If two values are identical, we cannot store them in differents groups, because however the split threshold would not be respected
+                    if continuous_values[threshold-1][0] == continuous_values[threshold][0]:
+                        threshold += 1
+                        continue                    
+                    
+                    if threshold < self.min_split_points or n - threshold < self.min_split_points:
+                        threshold += 1
                         continue
-                    child1 = PointSet(child1_features,child1_labels,self.types)
-                    child2 = PointSet(child2_features,child2_labels,self.types)
-                    gini1 = child1.get_gini()
-                    gini2 = child2.get_gini()
-                    n1 = len(child1.labels)
-                    n2 = len(child2.labels)
+                    
+                    n1 = threshold
+                    n2 = n - threshold
+                    
+                    gini1 = 1 - ((label_counter['left_false'])**2 + (label_counter['left_true'])**2)/((label_counter['left_false']+label_counter['left_true'])**2)
+                    gini2 = 1 - ((label_counter['right_false'])**2 + (label_counter['right_true'])**2)/((label_counter['right_false']+label_counter['right_true'])**2)
                     gini_split = (n1/n)*gini1 + (n2/n)*gini2
-                    gini_gain = self.get_gini() - gini_split
+
+                    gini_gain = gini_start - gini_split
                     if gini_gain > current_max_gini:
-                        current_value_maximizing =  (max([child1_features[k][feature_index] for k in range(len(child1_features))]) + min([child2_features[k][feature_index] for k in range(len(child2_features))]))/2
+                        current_value_maximizing = (continuous_values[threshold-1][0] + continuous_values[threshold][0])/2
                         current_max_gini = gini_gain
+                    threshold += 1
                 gini_results.append((feature_index, round(current_max_gini,7)))
                 self.maximizing_feature[feature_index] = current_value_maximizing
             
@@ -187,3 +199,14 @@ class PointSet:
             return None
         else:
             return self.maximizing_feature[feature_index] #The categorical and continuous features are handled the same way, with the dictionnary
+
+    def get_gini_2(self, values):
+        label_frequency = [0,0]
+        for label in values:
+            if label[1] == 0:
+                label_frequency[0] += 1
+            else:
+                label_frequency[1] += 1
+        point_count = len(values)
+        label_frequency = list(map(lambda x:x/point_count, label_frequency))
+        return 1 - label_frequency[0]**2 - label_frequency[1]**2
